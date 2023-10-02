cfbaf1372159cabef161ec90b0821d36325574df
Mattias Rådemar <mattiasrademar@gmail.com>
2023-10-02 20:06:20 +0200

    Some minor refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use regex::Regex;
 use sha256::digest;
 use std::collections::HashMap;
+use std::ffi::OsStr;
 use std::fs::{self, File};
 use std::io::{self, prelude::*};
+use std::path::PathBuf;
 
 // Replace with current dir when running
-const CLIENT_PATH: &'static str = "C:/projects/kcrp_string_hash/src/client_index.js";
-const SERVER_PATH: &'static str = "C:/projects/kcrp_string_hash/src/server_index.js";
-const CEF_PATH: &'static str = "C:/projects/kcrp_string_hash/src/cef_index.js";
+const CLIENT_PATH: &'static str = "C:/projects/kcrp_string_hash/test_data/client_index.js";
+const SERVER_PATH: &'static str = "C:/projects/kcrp_string_hash/test_data/server_index.js";
+const CEF_PATH: &'static str = "C:/projects/kcrp_string_hash/test_data/cef_index.js";
 
 struct EventHash {
     event: String,
@@ -15,8 +17,8 @@ struct EventHash {
 }
 
 impl EventHash {
-    fn new(event: &str, hay: &HashMap<String, String>) -> EventHash {
-        EventHash {
+    fn new(event: &str, hay: &HashMap<String, String>) -> Self {
+        Self {
             event: event.to_string(),
             hash: get_value_or_digest_sha256(event, hay),
         }
@@ -28,6 +30,8 @@ fn main() {
     let server_content = fs::read_to_string(SERVER_PATH).expect("Unable to read the file");
     let cef_content = fs::read_to_string(CEF_PATH).expect("Unable to read the file");
 
+    let re = Regex::new(r#"("server:[a-zA-Z-0-9:-_]*\")|(\"client:[a-zA-Z-0-9:-_]*\")"#).unwrap();
+
     let mut event_hashes: HashMap<String, String> = HashMap::new();
 
     fill_hash_map(
@@ -35,17 +39,22 @@ fn main() {
         server_content,
         cef_content,
         &mut event_hashes,
+        &re,
     );
 
-    let paths: Vec<&str> = vec![CLIENT_PATH, SERVER_PATH, CEF_PATH];
+    let paths: Vec<PathBuf> = vec![
+        PathBuf::from(CLIENT_PATH),
+        PathBuf::from(SERVER_PATH),
+        PathBuf::from(CEF_PATH),
+    ];
     for path in paths {
-        let _ = replace_event_names_in_files_with_hashes(&event_hashes, &path);
+        let _ = replace_event_names_in_files_with_hashes(&event_hashes, path.as_os_str());
     }
 }
 
 fn replace_event_names_in_files_with_hashes(
     event_hashes: &HashMap<String, String>,
-    file_path: &str,
+    file_path: &OsStr,
 ) -> Result<(), io::Error> {
     let mut file = File::open(file_path)?;
 
@@ -64,22 +73,22 @@ fn replace_event_names_in_files_with_hashes(
     Ok(())
 }
 
-fn fill_hash_map(
+fn fill_hash_map<'a>(
     client_content: String,
     server_content: String,
     cef_content: String,
     hash_map: &mut HashMap<String, String>,
+    re: &'a Regex,
 ) {
-    let client_matches = return_matches_from_lines(&client_content);
-    let server_matches = return_matches_from_lines(&server_content);
-    let cef_matches = return_matches_from_lines(&cef_content);
+    let client_matches = return_matches_from_lines(&client_content, &re);
+    let server_matches = return_matches_from_lines(&server_content, &re);
+    let cef_matches = return_matches_from_lines(&cef_content, &re);
 
     read_matches_and_insert_to_hash_map([client_matches, server_matches, cef_matches], hash_map);
 }
 
-fn return_matches_from_lines(lines: &str) -> Vec<&str> {
-    let re = Regex::new(r#"("server:[a-zA-Z-0-9:-_]*\")|(\"client:[a-zA-Z-0-9:-_]*\")"#).unwrap();
-    let matches: Vec<&str> = re.find_iter(&lines).map(|m| m.as_str()).collect();
+fn return_matches_from_lines<'a>(lines: &'a str, re: &Regex) -> Vec<&'a str> {
+    let matches: Vec<&'a str> = re.find_iter(&lines).map(|m| m.as_str()).collect();
     matches
 }
 
@@ -89,7 +98,7 @@ fn read_matches_and_insert_to_hash_map(
 ) {
     for m in matches {
         for line in m {
-            let parsed_event = line.replace("\"", "");
+            let parsed_event: String = line.chars().filter(|&c| c != '"').collect();
             let event_hash = EventHash::new(&parsed_event, &event_hashes);
 
             event_hashes.insert(event_hash.event, event_hash.hash);
@@ -98,8 +107,7 @@ fn read_matches_and_insert_to_hash_map(
 }
 
 fn get_value_or_digest_sha256(event: &str, hay: &HashMap<String, String>) -> String {
-    let hash_to_return = hay.get(event);
-    match hash_to_return {
+    match hay.get(event) {
         None => digest(event),
         Some(e) => e.to_string(),
     }
